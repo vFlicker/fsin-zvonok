78ae53f0871f12ccc72f1d34a2ea82d889601c22
Vladislav Flicker <flickervladislav@gmail.com>
2021-11-10 17:15:17 +0200

    Адаптирует Card, Transaction, поправит Header

--- src/scripts/modules/spoiler.js
+++ src/scripts/modules/spoiler.js
@@ -1,17 +1,10 @@
-const card = document.querySelector('.card');
+const cards = document.querySelectorAll('.card');
 
-if (card) {
-  const cardTitles = card.querySelectorAll('.card__title');
-
-  const onCardTitleClick = (evt) => {
-    const cardHeader = evt.target.closest('.card__header');
-    const cardContent = evt.target
-      .closest('.card')
-      .querySelector('.card__content');
-
-    cardHeader.classList.toggle('card__header--active');
-    cardContent.classList.toggle('card__content--active');
+if (cards) {
+  const cardClick = (evt) => {
+    const currentCard = evt.target.closest('.card');
+    currentCard.classList.toggle('card--active');
   };
 
-  cardTitles.forEach((cardTitle) => cardTitle.addEventListener('click', onCardTitleClick));
+  cards.forEach((card) => card.addEventListener('click', cardClick));
 }
